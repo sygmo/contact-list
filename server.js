d0e04f285e2e0a36985328fe47bb80cd4859e784
sygmo <sydney.mercier91@gmail.com>
2017-01-31 14:03:46 -0600

    installed angular; created index.html

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var express = require("express"),
+var express = require('express'),
 	app = express();
 
-app.get('/', function (req, res) {
-	res.sent("Hello world from server.js")
-}); // set route to index page
+app.use(express.static(__dirname + "/public")); // static - javascript, html, and css files
 
 app.listen(3000);
 console.log("Server running on port 3000");
