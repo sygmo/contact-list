d251e9ae9236a24e1e8e9e81298f8e45e5dda235
sygmo <sydney.mercier91@gmail.com>
2017-01-31 14:19:49 -0600

    set up bootstrap

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express'),
+var express = require("express"),
 	app = express();
 
 app.use(express.static(__dirname + "/public")); // static - javascript, html, and css files
+app.use(express.static(__dirname + "/bower_components")); // angular files
 
 app.listen(3000);
 console.log("Server running on port 3000");
